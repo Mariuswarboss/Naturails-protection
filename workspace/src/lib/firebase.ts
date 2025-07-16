ef6cb05b3fefb6464dbca4a232f5ba537093dbef
Marius Chistrea <mariuswarboss@gmail.com>
2025-07-16 20:52:12 +0000

    Backup from Firebase Studio

--- workspace/src/lib/firebase.ts
+++ workspace/src/lib/firebase.ts
@@ -1,19 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAsB5FS5IhBb3900uIWKJbq0LP5SyQLWA8",
+  authDomain: "ecoshop-moldova.firebaseapp.com",
+  projectId: "ecoshop-moldova",
+  storageBucket: "ecoshop-moldova.firebasestorage.app",
+  messagingSenderId: "1060645000148",
+  appId: "1:1060645000148:web:c61ca11b63a26583757c41"
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-export default app;
+const app = initializeApp(firebaseConfig);
